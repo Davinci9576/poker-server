78226d0fa8c411fd5295fd0154e1effdaf97bc7f
Irmvvn_D_Vinci <irmuun6789@gmail.com>
2026-04-26 15:31:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -32,6 +32,8 @@ app.get("/leaderboard", (req, res) => {
     res.json(sorted);
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running");
 });
